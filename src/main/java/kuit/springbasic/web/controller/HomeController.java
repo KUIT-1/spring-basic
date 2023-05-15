f21613c9d0aeea0460fb7cf69241574cbac84d3f
hhj <gudwn357@gmail.com>
2023-05-15 18:18:39 +0900

    feat (HomeController) : 롬복 활용해 구현

--- src/main/java/kuit/springbasic/web/controller/HomeController.java
+++ src/main/java/kuit/springbasic/web/controller/HomeController.java
@@ -1,17 +1,29 @@
 package kuit.springbasic.web.controller;
 
+import kuit.springbasic.web.dao.QuestionDao;
+import kuit.springbasic.web.domain.Question;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Slf4j
+@Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final QuestionDao questionDAO = new QuestionDao();
 
     @RequestMapping("/")
     public String showHome(Model model) {
         log.info("HomeController.showHome");
 
         // 코드 추가 필요
+        List<Question> questions = questionDAO.findAll();
+        model.addAttribute("questions", questions);
 
         return "home";
     }
