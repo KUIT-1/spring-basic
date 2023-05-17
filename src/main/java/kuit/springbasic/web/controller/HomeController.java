446c635e231f8b2ac2f885755f85adee0a7e2ee9
versatile0010 <siam0294@naver.com>
2023-05-17 23:23:03 +0900

    feat: questionDao, domain.question 생성 및 HomeControllerV1 구현

--- src/main/java/kuit/springbasic/web/controller/HomeController.java
+++ src/main/java/kuit/springbasic/web/controller/HomeController.java
@@ -1,19 +1,35 @@
 package kuit.springbasic.web.controller;
 
+import kuit.springbasic.web.dao.QuestionDao;
+import kuit.springbasic.web.domain.Question;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
+@Controller
 @Slf4j
 public class HomeController {
+    private final QuestionDao questionDao;
+
+    @Autowired
+    public HomeController(QuestionDao questionDao) {
+        this.questionDao = questionDao;
+    }
 
     @RequestMapping("/")
-    public String showHome(Model model) {
+    public ModelAndView showHome() {
         log.info("HomeController.showHome");
+        ModelAndView modelAndView = new ModelAndView("/home");
+        List<Question> questions = questionDao.findAll();
+        modelAndView.addObject("questions", questions);
 
-        // 코드 추가 필요
-
-        return "home";
+        return modelAndView;
     }
 
 }
