1149b71050a1e273ded5d9907337e5b6543d9b84
lyouxsun <leedrkr323@konkuk.ac.kr>
2023-05-19 13:20:51 +0900

    미션1

--- src/main/java/kuit/springbasic/web/controller/HomeController.java
+++ src/main/java/kuit/springbasic/web/controller/HomeController.java
@@ -1,19 +1,43 @@
 package kuit.springbasic.web.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
+import kuit.springbasic.web.dao.QuestionDao;
+import kuit.springbasic.web.domain.Question;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
+@Controller
 @Slf4j
+@RequiredArgsConstructor
 public class HomeController {
+    private final QuestionDao questionDao;
+
+
+    //@RequestMapping("/")
+    public ModelAndView showHomeV1() {
+        log.info("HomeController.showHomeV1");
+
+        ModelAndView modelAndView = new ModelAndView("/home");
+
+        List<Question> questions = questionDao.findAll();
+        modelAndView.addObject("questions", questions);
+
+        return modelAndView;
+    }
 
     @RequestMapping("/")
-    public String showHome(Model model) {
-        log.info("HomeController.showHome");
+    public String showHomeV2(Model model){
+        log.info("HomeController.showHomeV2");
 
-        // 코드 추가 필요
+        List<Question> questions = questionDao.findAll();
+        model.addAttribute("questions", questions);
 
-        return "home";
+        return "/home";
     }
 
 }
